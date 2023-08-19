2cff0b422b1222eefd4f6a468c981bdb8bec9ba7
kamione <yatyat0321@gmail.com>
2023-08-19 08:49:05 +0800

    [new]

---
+++ src/R/stats.R
@@ -0,0 +1,13 @@
+confidence_interval <- function(vector, interval = 0.95) {
+    # Standard deviation of sample
+    vec_sd <- sd(vector)
+    # Sample size
+    n <- length(vector)
+    # Mean of sample
+    vec_mean <- mean(vector)
+    # Error according to t distribution
+    error <- qt((interval + 1)/2, df = n - 1) * vec_sd / sqrt(n)
+    # Confidence interval as a vector
+    result <- c("lower" = vec_mean - error, "upper" = vec_mean + error)
+    return(result)
+}
